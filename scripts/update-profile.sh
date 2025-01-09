3ae8aeda1281e1d387ff079c61aa1f9cb074207b
balsama <adam.balsam@acquia.com>
2025-01-09 09:14:39 -0500

    Improve readability of profile update script

--- scripts/update-profile.sh
+++ scripts/update-profile.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
-wget https://git.drupalcode.org/project/drupal_cms/-/archive/1.x/drupal_cms-1.x.tar.gz -O drupal_cms_installer.tar.gz
-tar -xf drupal_cms_installer.tar.gz -C docroot/profiles/custom/
-rm -rf docroot/profiles/custom/drupal_cms_installer
-mv docroot/profiles/custom/drupal_cms-1.x docroot/profiles/custom/drupal_cms_installer
-rm drupal_cms_installer.tar.gz
+set -e
+
+PROFILE_DIR="docroot/profiles/custom/drupal_cms_installer"
+ARCHIVE_URL="https://git.drupalcode.org/project/drupal_cms/-/archive/1.x/drupal_cms-1.x.tar.gz?path=project_template/web/profiles/drupal_cms_installer"
+ARCHIVE_NAME="drupal_cms_installer.tar.gz"
+
+wget "$ARCHIVE_URL" -O "$ARCHIVE_NAME"
+tar -xf "$ARCHIVE_NAME" -C docroot/profiles/custom/
+
+rm -rf "$PROFILE_DIR"
+mv docroot/profiles/custom/drupal_cms-1.x-project_template-web-profiles-drupal_cms_installer/project_template/web/profiles/drupal_cms_installer "$PROFILE_DIR"
+
+rm -rf docroot/profiles/custom/drupal_cms-1.x-project_template-web-profiles-drupal_cms_installer
+rm "$ARCHIVE_NAME"
