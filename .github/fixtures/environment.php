0dcf0d33e5a6ea78b690ee271e111d642a0f4550
balsama <adam.balsam@acquia.com>
2025-02-25 09:44:58 -0500

    Remove debug

--- .github/fixtures/environment.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Acquia\Drupal\RecommendedSettings\Helpers\EnvironmentDetector;
-
-include_once __DIR__ . '/../../vendor/autoload.php';
-
-$foo = EnvironmentDetector::getEnvironments();
-print_r($foo);
--- .github/fixtures/gh-actions.settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $databases['default']['replica'][] = [
-    'driver' => 'mysql',
-    'database' => 'drupal',
-    'username' => 'root',
-    'password' => 'drupal',
-    'host' => 3306,
-  ];
