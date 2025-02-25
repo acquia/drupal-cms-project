61e9a1f27a17b2d2925f584b9945b3d112efb95a
balsama <adam.balsam@acquia.com>
2025-02-24 23:28:47 -0500

    toying with settings

---
+++ .github/fixtures/gh-actions.settings.php
@@ -0,0 +1,8 @@
+<?php
+  $databases['default']['replica'][] = [
+    'driver' => 'mysql',
+    'database' => 'drupal',
+    'username' => 'root',
+    'password' => 'drupal',
+    'host' => 3306,
+  ];
