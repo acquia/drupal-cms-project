2b2de2c785025c5b7b5b1d330ea081470500aaa4
adam balsam <adam.balsam@acquia.com>
2026-04-27 06:58:47 -0400

    Switch to prod URLs if prod env var is present

---
+++ docroot/modules/custom/acquia_id/src/AcquiaIdServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\acquia_id;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\DependencyInjection\ServiceProviderBase;
+
+final class AcquiaIdServiceProvider extends ServiceProviderBase {
+
+  public function alter(ContainerBuilder $container): void {
+    parent::alter($container);
+
+    if (getenv('AH_SITE_ENVIRONMENT') === 'prod') {
+      $idp_base_uri = 'https://id.acquia.com/oauth2/default';
+      $cloud_api_base_uri = 'https://cloud.acquia.com';
+    }
+    else {
+      $idp_base_uri = 'https://staging.id.acquia.com/oauth2/default';
+      $cloud_api_base_uri = 'https://staging.cloud.acquia.com';
+    }
+
+    $container->setParameter('acquia_id.idp_base_uri', $idp_base_uri);
+    $container->setParameter('acquia_id.cloud_api_base_uri', $cloud_api_base_uri);
+    $container->setParameter('acquia_id.idp_logout_redirect_uri', $cloud_api_base_uri);
+  }
+
+}
