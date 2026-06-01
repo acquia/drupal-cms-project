345e9a7d7da0267bc903cd4ebe895e35aa863937
rujutaghanekar <rujuta.ghanekar@gmail.com>
2026-06-02 01:46:49 +0530

    fix cmd

--- hooks/common/post-code-update/config-import.sh
+++ hooks/common/post-code-update/config-import.sh
@@ -4,14 +4,8 @@
 #
 # Run drush config:import in all environments post code deploy.
 
-site="$1"
-target_env="$2"
-
-# Use vendor drush if available, otherwise fall back to drush9
-drush='drush9'
-if [ -e /var/www/html/vendor/bin/drush ]; then
-  drush='/var/www/html/vendor/bin/drush'
-fi
+site=$1
+target_env=$2
 
 echo "Importing config for $site.$target_env"
-$drush @$site.$target_env cim -y
+./vendor/bin/drush @$site.$target_env config:import -y
