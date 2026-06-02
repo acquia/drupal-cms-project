e73a35f97824914bf7981dab2d9245a0c33fb30c
adam balsam <adam.balsam@acquia.com>
2026-06-01 21:00:43 -0400

    Only run config import and DB updates if Drush is already installed

--- hooks/common/post-code-update/config-import.sh
+++ hooks/common/post-code-update/config-import.sh
@@ -2,11 +2,14 @@
 #
 # Cloud Hook: Import config
 #
-# Run drush config:import in all environments post code deploy.
+# Run drush config:import in all environments post code update.
 
 # Map the script inputs to convenient names.
 site=$1
 target_env=$2
 drush_alias=$site'.'$target_env
 
-drush @$drush_alias config:import --yes
+if drush @$drush_alias status 2>/dev/null | grep -q "Successful"; then
+  drush @$drush_alias updatedb --yes
+  drush @$drush_alias config:import --yes
+fi
